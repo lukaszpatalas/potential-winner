6113a68b6f8c0143e3fbfb0a4433dc67287a1cee
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-06-27 19:54:22 +0100

    Add javascript function and few tweaks to code

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 "use strict"
+
+const testButton = document.querySelector('.test-button');
+testButton.addEventListener('click', updateText);
+
+function updateText() {
+    if (testButton.textContent === "Click me") {
+        testButton.textContent = "Unclick me";
+    } else {
+        testButton.textContent = "Click me";
+    }
+}
