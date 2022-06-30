110a0da275736b0edf2c47868afddb3bc53b50dd
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-06-30 20:47:23 +0100

    Add quotes to site depending on user choice

--- javascript.js
+++ javascript.js
@@ -11,3 +11,24 @@ function updateText() {
     }
 }
 
+
+const game = document.querySelector('select');
+const quote = document.querySelector('p');
+
+game.addEventListener('change', setGame);
+
+function setGame() {
+  const choice = game.value;
+
+  if (choice === 'fallout') {
+    quote.textContent = 'War, war never changes...';
+  } else if (choice === 'witcher') {
+    quote.textContent = 'I will carry on killing monsters in the ruins of this world until some monster kills me.';
+  } else if (choice === 'cyberpunk') {
+    quote.textContent = 'The brightest light casts the darkest shadow.';
+  } else if (choice === 'wow') {
+    quote.textContent = 'You are not prepared!';
+  } else {
+    quote.textContent = '';
+  }
+}
